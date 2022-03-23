bd7e7032a618c86026bea6fb7acfc0007f9c21d0
Deependra-Negi <916deepaknegi@gmail.com>
2022-03-23 12:58:46 +0530

    scorecard added

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import styles from './Greeting.module.css';
+import ScoreCard from './ScoreCard';
 
 function Greeting() {
 
   const [flag, setFlag] = useState(true)
+  const [Change, setChange] = useState(false)
   
   function handleClick() {
     setFlag(!flag);
+    setTimeout(()=>{setChange(true)},1000)
   }
   let obj2 = {
     opacity: "0",
@@ -18,27 +21,27 @@ function Greeting() {
 }
 
   return (
-    <>
-      <div style={flag?obj1:obj2}>
+    <>{Change ? <ScoreCard /> :
+      <div style={flag ? obj1 : obj2}>
         <div className={styles.cont}>
           <div className={styles.rocket}>
-            <img src="rocket.png" alt="rocket"/>
+            <img src="rocket.png" alt="rocket" />
           </div>
-            <div className={styles.bottom}>
-              <div><h2>Congratulations!</h2></div>
-              <div>
-                <p>
-                  You have completed the quiz. Make sure to check
-                  your inbox for any future updates from the company.
-                </p>
-              </div>
-              <div>
-                <button onClick={handleClick}>Continue to results</button>
-              </div>
+          <div className={styles.bottom}>
+            <div><h2>Congratulations!</h2></div>
+            <div>
+              <p>
+                You have completed the quiz. Make sure to check
+                your inbox for any future updates from the company.
+              </p>
             </div>
+            <div>
+              <button onClick={handleClick}>Continue to results</button>
+            </div>
+          </div>
         </div>
-        </div>
-      </>
+      </div>}
+    </>
   )
 }
 
--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import styles from './ScoreCard.module.css'
 
 function ScoreCard() {
   return (
-    <div>ScoreCard</div>
+    <>
+      <div className={styles.cont}>
+        <div>
+          <div className={styles.upper}>
+            <div>
+              <img src="avatar.png" alt="avatar" />
+            </div>
+            <div>
+              <img src="trophy.png" alt="trophy" />
+            </div>
+          </div>
+          <div className={styles.textCont}>
+            <h2>Kelvin Malone</h2>
+            <p>Hooray! You have what it takes to be a Kickass Sales Ninja</p>
+          </div>
+        </div> 
+
+        <div className={styles.bottomCont}>
+          <div className={styles.scoreCont}>
+            <div>
+              <p>Overall Quiz Score</p>
+              <h2>86%</h2>
+            </div>
+            <div>
+              <img src="progress.png" alt="progress" />
+              <span>🔥</span>
+            </div>
+          </div>
+          <div className={styles.profileCont}>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
